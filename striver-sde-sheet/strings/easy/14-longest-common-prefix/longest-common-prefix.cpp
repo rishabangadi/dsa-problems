5b74e108331fe4e435573011b4dabfda1dd3a88c
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-01-24 12:44:57 +0530

    Time: 3 ms (73.43%) | Memory: 10.6 MB (11.82%) - LeetSync

--- striver-sde-sheet/strings/easy/14-longest-common-prefix/longest-common-prefix.cpp
+++ striver-sde-sheet/strings/easy/14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,15 +3,15 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.size() == 0)
             return "";
-        string prefix = strs[0];
-        for (int i = 1; i < strs.size(); i++) {
-            while (strs[i].find(prefix) != 0) {
-                prefix = prefix.substr(0, prefix.size() - 1);
-                if (prefix.empty())
-                    return "";
+
+        for (int i = 0; i < strs[0].size(); i++) {
+            char c = strs[0][i];
+            for (int j = 1; j < strs.size(); j++) {
+                if (i == strs[j].size() || strs[j][i] != c)
+                    return strs[0].substr(0, i);
             }
         }
 
-        return prefix;
+        return strs[0];
     }
 };
