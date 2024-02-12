462e269bf219aa1a0f32dde0552bbb5b72ec4a6c
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-02-12 15:18:11 +0530

    Time: 15 ms (53.21%) | Memory: 22.1 MB (18.20%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Hashing/169-majority-element/majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == candidate)
+                count++;
+            else
+                count--;
+            if (count <= 0) {
+                count = 1;
+                candidate = nums[i];
+            }
+        }
+        return candidate;
+    }
+};
