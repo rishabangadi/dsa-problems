42ded3ca4c49ddf422f544e2554cd360cf7cb0a0
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-02-12 10:12:34 +0530

    Time: 221 ms (87.78%) | Memory: 20.8 MB (10.43%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Hashing/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        counts = defaultdict(int)
+        counts[0] = 1
+        ans = 0
+        curr = 0
+        for num in nums:
+            curr += num
+            ans += counts[curr - k]
+            counts[curr] += 1
+        return ans
