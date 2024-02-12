28454b149636dae2048be1889256364cbe237afc
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-02-12 08:57:02 +0530

    Time: 59 ms (89.83%) | Memory: 17 MB (79.25%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Hashing/2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        counts = defaultdict(int)
+        for arr in nums:
+            for x in arr:
+                counts[x] += 1
+        n = len(nums)
+        ans = []
+        for key in counts:
+            if counts[key] == n:
+                ans.append(key)
+        return sorted(ans)
