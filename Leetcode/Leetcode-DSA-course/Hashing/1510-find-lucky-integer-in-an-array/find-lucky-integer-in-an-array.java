b95bc974a3da225308db79123c59eeff64cd9de3
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-02-13 10:11:10 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (20.96%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Hashing/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int freq[] = new int[501];
+        for (int num : arr)
+            freq[num]++;
+        for (int i = 500; i >= 1; i--) {
+            if (freq[i] == i)
+                return i;
+        }
+        return -1;
+    }
+}
