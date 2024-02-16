c87c3a28981b969e56ec567e1117434c7a395eb8
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-02-16 20:12:44 +0530

    Time: 62 ms (64.25%) | Memory: 51.8 MB (44.04%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Linked-List/82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let sentinel = new ListNode(0, head);
+    let pred = sentinel;
+    while (head != null) {
+        if (head.next != null && head.val == head.next.val) {
+            while (head.next != null && head.val == head.next.val)
+                head = head.next;
+            pred.next = head.next;
+        }
+        else {
+            pred = pred.next;
+        }
+        head = head.next;
+    }
+    return sentinel.next;
+};
