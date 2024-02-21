b55862a6837d4828bfdffc6debfe90693c79a587
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-02-21 11:52:18 +0530

    Time: 154 ms (95.83%) | Memory: 137.1 MB (41.22%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Stacks-and-Queues/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> queue;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!queue.empty() && nums[i] >= nums[queue.back()])
+                queue.pop_back();
+            queue.push_back(i);
+            while (queue.front() <= i - k)
+                queue.pop_front();
+            if (i >= k - 1)
+                ans.push_back(nums[queue.front()]);
+        }
+        return ans;
+    }
+};
