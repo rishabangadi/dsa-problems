8718050d2a6a7f093557c47f68551f520c71208a
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-02-28 19:23:05 +0530

    Time: 74 ms (58.70%) | Memory: 59.7 MB (48.81%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Trees-and-Graphs/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    if (!root)
+        return null;
+    if (root == p || root == q)
+        return root;
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if (left && right)
+        return root;
+    if (left)
+        return left;
+    return right;
+
+};
