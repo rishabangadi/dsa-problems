5237328d10654b53b63d59cfca808098745f9bdc
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-04-01 20:51:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (87.89%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Trees-and-Graphs/58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        vector<string> words;
+        int left = 0;
+        int n = s.length();
+        while(left<n && s[left]==' ')
+            left++;
+        int right = n-1;
+        while(right>=left && s[right]==' ')
+            right--;
+        int count = 0;
+        while(right>=left){
+            if(s[right]!=' '){
+                count++;
+                right--;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+
+    }
+};
