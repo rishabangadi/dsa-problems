f0b22f25cbdda9925d1f62c785cf7bbf02fb0f95
Rishab B Angadi <45505864+rishabangadi@users.noreply.github.com>
2024-07-27 20:01:49 +0530

    Time: 143 ms (57.41%) | Memory: 93.5 MB (41.19%) - LeetSync

---
+++ Leetcode/Leetcode-DSA-course/Dynamic-Programming/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector(2, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int holding = 0; holding < 2; holding++) {
+                int ans = dp[i + 1][holding];
+                if (holding == 0)
+                    ans = max(ans, -prices[i] + dp[i + 1][1]);
+                else
+                    ans = max(ans, prices[i] - fee + dp[i + 1][0]);
+                dp[i][holding] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+};
